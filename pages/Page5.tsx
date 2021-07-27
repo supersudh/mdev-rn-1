e08269474c4b91f310e90c40ee7adc9dbe9b3b4b
HK7275 <73687452+HK7275@users.noreply.github.com>
2021-07-27 18:24:45 -0400

    Update Page5.tsx

--- pages/Page5.tsx
+++ pages/Page5.tsx
@@ -3,14 +3,39 @@ import { StyleSheet, Text, View } from 'react-native';
 
 const Page5 = () => {
   return (
-    <View>
-      <Text>Page5</Text>
-    </View>
+    <SafeAreaView style = {styles.wrapper}>
+            <View style = {styles.element1}/>
+            <View style = {styles.element1}/>
+            <View style = {styles.element1}/>
+            <View style = {styles.element1}/>
+            <View style = {styles.element2}/>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  
+  wrapper:{
+        flex:1,
+        flexDirection:'column',
+        backgroundColor:'white',
+        flexWrap:'wrap',
+        marginLeft:10
+    },
+    element1:{
+        backgroundColor:'#07afd9',
+        width:400,
+        height:110,   
+        margin:5
+    },
+    element2:{
+        backgroundColor:'#7253e6',
+        width:100,
+        height:100,
+        position:'absolute',
+        bottom:0,
+        right:15,
+        marginBottom: 45
+    },
 });
 
 export default Page5;
