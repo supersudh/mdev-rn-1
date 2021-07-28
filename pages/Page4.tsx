736aa2fcb8eb38590bb129b222e2e85777cd32e8
Haripatel279 <42485762+Haripatel279@users.noreply.github.com>
2021-07-28 19:10:10 +0530

    Update Page4.tsx

--- pages/Page4.tsx
+++ pages/Page4.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, SafeAreaView, Platform } from "react-native";
 
 const Page4 = () => {
-  return (
-    <View>
-      <Text>Page4</Text>
-    </View>
-  );
-};
+    return (
+        <SafeAreaView style = {styles.wrapper}>
+            <View style = {styles.box1}/>
+            <View style = {styles.box1}/>
+            <View style = {styles.box1}/>
+            <View style = {styles.box1}/>
+            <View style = {styles.box1}/>
+            <View style = {styles.box1}/>
+            <View style = {styles.box1}/>
+            <View style = {styles.box1}/>
+            <View style = {styles.box1}/>
+            <View style = {styles.box2}/>
+        </SafeAreaView>
+    )
+}
 
 const styles = StyleSheet.create({
-  
-});
+    wrapper:{
+        flex:1,
+        flexDirection:'row',
+        backgroundColor:'white',
+        flexWrap:'wrap',
+        marginLeft:25
+    },
+    box1:{
+        backgroundColor:'blue',
+        width:110,
+        height:110,   
+        margin:5
+    },
+    box2:{
+        backgroundColor:'green',
+        width:100,
+        height:100,
+        position:'absolute',
+        bottom:0,
+        right:15,
+        marginBottom:20
+    },
+})
 
 export default Page4;
